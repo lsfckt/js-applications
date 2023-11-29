ec7b02c6098e76f758d47022943dc57a87775487
lsfckt <132505560+lsfckt@users.noreply.github.com>
2023-11-29 23:09:07 +0200

    half solution of book-library

--- Data and Authentication EXERCISE/04.Book-library/app.js
+++ Data and Authentication EXERCISE/04.Book-library/app.js
@@ -1,1 +1,38 @@
-console.log('My requests...')
+function solve() {
+    // write get functionality and other tasks
+    
+    const baseUrl = 'http://localhost:3030/jsonstore/collections/books';
+    const form = document.querySelector('form');
+    form.addEventListener('submit', createBook);
+
+    async function createBook(e) {
+        e.preventDefault();
+        try {
+            const formData = new FormData(form);
+            const title = formData.get('title');
+            const author = formData.get('author');
+
+            if(!title || !author) {
+                throw new Error('Empty form, please write author and title');
+            }
+
+            const createRes = await fetch(baseUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    author,
+                    title,
+                }),
+            });
+
+            const resData = await createRes.json();
+
+            if(!createRes.ok) {
+                throw new Error(createRes.status);
+            }
+        } catch (error) {
+            alert(error);
+        };
+    };
+};
+solve();
