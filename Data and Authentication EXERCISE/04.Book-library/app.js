747ed14f0c4fd05eac8f6c2fd399419b2ea7bc05
lsfckt <stoyanovkt@gmail.com>
2023-12-01 16:48:46 +0200

    half solution of fisher game

--- Data and Authentication EXERCISE/04.Book-library/app.js
+++ Data and Authentication EXERCISE/04.Book-library/app.js
@@ -1,107 +1,126 @@
-function solve() {
-    // write get functionality and other tasks
+const state = {};
 
-    const baseUrl = 'http://localhost:3030/jsonstore/collections/books';
-    const form = document.querySelector('form');
-    form.addEventListener('submit', createBook);
+async function request(url, options) {
+    const response = await fetch(url, options);
 
-    const tbody = document.querySelector('tbody');
-
-    async function createBook(e) {
-        e.preventDefault();
-        try {
-            const formData = new FormData(form);
-            const title = formData.get('title');
-            const author = formData.get('author');
-
-            if (!title || !author) {
-                throw new Error('Empty form, please write author and title');
-            }
-
-            const createRes = await fetch(baseUrl, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    author,
-                    title,
-                }),
-            });
-
-            const resData = await createRes.json();
-
-            if (!createRes.ok) {
-                throw new Error(createRes.status);
-            }
-
-            const values = Object.values(resData);
-
-            const tr = document.createElement('tr');
-            const id = values[2];
-            tr.id = id;
-
-
-            const td = '';
-
-            values.forEach(td => {
-                td = document.createElement('td');
-                tr.appendChild(td);
-            });
-
-            tr.children[0].textContent = title
-            tr.children[1].textContent = author
-
-            const editBtn = document.createElement('button');
-            editBtn.id = 'edit';
-            editBtn.textContent = 'Edit';
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.id = 'delte';
-            deleteBtn.textContent = 'Delete';
-            const lastTd = tr.children[2];
-
-            lastTd.append(editBtn, deleteBtn);
-            tbody.appendChild(tr);
-
-        } catch (error) {
-            alert(error);
-        };
-    };
-    
-    function updateBook() {
-        const editBtns = document.querySelectorAll('body>table>tbody>tr>td>button');
-
-        editBtns.forEach(currBtn => {
-            currBtn.addEventListener('click', async (e) => {
-                e.preventDefault();
-
-                let parentEl = e.target.parentElement.parentElement;
-                const titleEl = parentEl.children[0];
-                const authorEl = parentEl.children[1];
-    
-                let editTitle = titleEl.textContent;
-                let editAuthor = authorEl.textContent;
-    
-                const h3 = document.querySelector('h3');
-                h3.textContent = 'Edit FORM';
-                const submitBtn = form[2].textContent = 'Save';
-    
-                const editRes = await fetch(baseUrl + `/${parentEl.id}`, {
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        editTitle,
-                        editAuthor,
-                    }),
-                });
-    
-                const editData = await editRes.json();
-                console.log(editData);
-                titleEl.textContent = editData.title;
-                editAuthor.textContent = editData.author;
-            });
-        });
+    if (response.ok === false) {
+        const error = await response.json();
+        alert(error.message);
+        throw new Error(error.message);
     }
-    updateBook();
-    
+
+    const data = await response.json();
+    return data;
 }
-solve();
+
+async function getAllBooks() {
+    const books = await request('http://localhost:3030/jsonstore/collections/books');
+    state.books = books;
+    const bookRows = Object.entries(books).map(createBookRow).join('')
+    document.querySelector('tbody').innerHTML = bookRows;
+
+    function createBookRow([id, book]) {
+        const result = `
+        <tr data-id="${id}">
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>
+                <button class="editBtn">Edit</button>
+                <button class="deleteBtn">Delete</button>
+            </td>
+        </tr>`;
+
+        return result;
+    }
+}
+
+async function createBook(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const book = {
+        title: formData.get('title'),
+        author: formData.get('author')
+    };
+
+    await request('http://localhost:3030/jsonstore/collections/books', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(book)
+    });
+
+    event.target.reset();
+
+    getAllBooks();
+}
+
+async function updateBook(event) {
+    const formData = new FormData(event.target.parentNode);
+    const id = formData.get('id');
+    const book = {
+        title: formData.get('title'),
+        author: formData.get('author')
+    };
+
+    await request('http://localhost:3030/jsonstore/collections/books/' + id, {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(book)
+    });
+
+    document.getElementById('createForm').style.display = 'block';
+    document.getElementById('editForm').style.display = 'none';
+    event.target.parentNode.reset();
+
+    getAllBooks();
+}
+
+async function deleteBook(id) {
+    await request('http://localhost:3030/jsonstore/collections/books/' + id, {
+        method: 'delete',
+    });
+
+    getAllBooks();
+}
+
+function start() {
+    document.getElementById('loadBooks').addEventListener('click', getAllBooks);
+    document.getElementById('createForm').addEventListener('submit', createBook);
+    document.querySelector('#editForm [value="Save"]').addEventListener('click', updateBook);
+    document.querySelector('#editForm [value="Cancel"]').addEventListener('click', (event) => {
+        event.target.parentNode.reset();
+        document.getElementById('createForm').style.display = 'block';
+        document.getElementById('editForm').style.display = 'none';
+    });
+    document.querySelector('table').addEventListener('click', handleTableClick)
+
+    getAllBooks();
+}
+
+function handleTableClick(event) {
+    // console.log(event.target);
+    if (event.target.className === 'editBtn') {
+        // console.log('clicked edit');
+        document.getElementById('createForm').style.display = 'none';
+        document.getElementById('editForm').style.display = 'block';
+
+        const bookId = event.target.parentNode.parentNode.dataset.id;
+        loadBookForEditing(bookId);
+
+    } else if (event.target.className === 'deleteBtn') {
+        const bookId = event.target.parentNode.parentNode.dataset.id;
+        const confirmDelete = confirm('Are you sure you want to delete given book?');
+        if (confirmDelete) {
+            deleteBook(bookId);
+            getAllBooks();
+        }
+    }
+}
+
+async function loadBookForEditing(bookId) {
+    const book = state.books[bookId];
+    document.querySelector('#editForm [name="id"]').value = bookId;
+    document.querySelector('#editForm [name="title"]').value = book.title;
+    document.querySelector('#editForm [name="author"]').value = book.author;
+}
+
+start();
--- Data and Authentication EXERCISE/05.Fisher-Game/src/register.js
+++ Data and Authentication EXERCISE/05.Fisher-Game/src/register.js
@@ -1,1 +1,52 @@
-console.log('TODO:// Implement Register functionality');
+const logout = document.getElementById('logout');
+logout.style.display = 'none';
+const url = 'http://localhost:3030/users/register';
+
+function register() {
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', onRegister);
+
+    async function onRegister(e) {
+        e.preventDefault();
+        try {
+            const formData = new FormData(form);
+            const email = formData.get('email');
+            const password = formData.get('password');
+            const rePass = formData.get('rePass');
+
+            if (!email || !password) {
+                document.getElementsByName('email')[0].value = '';
+                document.getElementsByName('password')[0].value = '';
+                document.getElementsByName('rePass')[0].value = '';
+                throw new Error('Empty email or passowrd field, please try again');
+            } else if (password !== rePass) {
+                throw new Error('Repeat password is different, please try again.')
+            }
+
+            const regRes = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
+            });
+            const regData = await regRes.json();
+
+            if (!regRes.ok) {
+                throw new Error(regRes.status, regRes.statusText);
+            }
+
+            sessionStorage.setItem('accessToken', regData.accessToken);
+            sessionStorage.setItem('email', regData.email);
+            sessionStorage.setItem('passowrd', regData.password);
+
+            window.location.href = 'index.html';
+        } catch (error) {
+            document.querySelector('p').textContent = error;
+        }
+    }
+
+}
+register();
